fa451c8120c65b7a14a6fac5d6b28edd40295028
GhitaBouzida <124607096+GhitaBouzida@users.noreply.github.com>
2023-02-23 14:45:59 +0100

    Add files via upload

---
+++ CHECK POINT 4.sql
@@ -0,0 +1,4 @@
+Insert into customer (id_cus, namee, adress) VALUES(1, "Alice","123 Main st"), (2,"Bob","456 Market st"),(3,"Charlie","789 Elm st");
+Insert into Product (id_pro, Price, Name) VALUES(1,1000, "Widget"), (2,2000,"Gadget"),(3,1500,"Dookickey");
+Insert into Orders (id_ord,id_cus,id_pro,Quantity,Datee) Values (1,1,1,10,"2021-01-01"),(2,1,2,5,"2021-01-02"),
+(3,2,1,3,"2021-01-03"),(4,2,2,7,"2021-01-04"), (5,3,1,2,"2021-01-05"),(6,3,3,3,"2021-01-06")
